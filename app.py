13853e04e55f2b50907b5ebb7d2319f31d4338a6
Alex Lemire <113059159+CyberPenguinAlpha@users.noreply.github.com>
2024-10-30 17:49:47 -0500

    Update app.py
    Attempt #2 to resolve key not found

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from flask import Flask, request, jsonify, render_template
 app = Flask(__name__)
 
 # Load API key from environment variable
-genai.configure(REACT_APP_KEY)
+genai.configure(os.environ.get(KEY))
 
 model = genai.GenerativeModel("gemini-1.5-flash")
 # Route for the welcome page (root URL)
