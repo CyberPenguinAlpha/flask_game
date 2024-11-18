8139b5b43fd59b21bc2b6d0fbad767e99397afff
Alex Lemire <113059159+CyberPenguinAlpha@users.noreply.github.com>
2024-11-18 15:12:21 -0600

    Update app.py
    ...and student response

--- app.py
+++ app.py
@@ -110,7 +110,7 @@ def evaluate_response(scenario, student_response): #TODO: impliment tokenization
     )
     return response.text
 
-#student_response="I crawl low and cover my face to escape safely!" #DEMO: NEED DYNAMIC AQUISITION FROM FRONTEND
+student_response="I crawl low and cover my face to escape safely!" #DEMO: NEED DYNAMIC AQUISITION FROM FRONTEND
 scenario_number=1 #DEMO: NEED DYNAMIIIC AQUISITION FROM FRONTEND
 print(evaluate_response(scenario_bank(scenario_number),student_response))
 
